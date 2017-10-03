5878d6a8f94c39ac785ab91d079c0f7404beb062
Sherwin32 <sherwin.shann@gmail.com>
2017-10-02 23:20:58 -0700

    done mend.js in a little bit silly way. Should've used the replace function

--- mend.js
+++ mend.js
@@ -8,8 +8,22 @@ var torn_clothes = [
 /* YOUR CODE HERE */
 function mend(clothing_item){
     // return mended_clothing_item;
+    function holeFilter(letter){
+    		var result = letter.indexOf("/") < 0;
+    		return result;
+    	}
+
+    var mended_clothing_item = clothing_item.map(function(item){
+    	item = item.split("");
+    	console.log(item);
+    	var mended_string = (item.filter(holeFilter)).join("");
+    	console.log(mended_string);
+    	return mended_string;
+    });
+    return mended_clothing_item;
 }
 
 
 /* PRINT RESULT */
 console.log("--> Finished running mend.js");
+console.log(mend(torn_clothes));
