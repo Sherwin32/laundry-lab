5d49c00a2aa649e11bc56f4ba38870f2529bad0d
Sherwin32 <sherwin.shann@gmail.com>
2017-10-02 23:24:42 -0700

    done clean.js with a piece of nice and clean code:)

--- clean.js
+++ clean.js
@@ -9,8 +9,13 @@ var dirty_clothes = [
 /* YOUR CODE HERE */
 function clean(clothing_item){
     // return clean_clothing_item;
+    var clean_clothing_item = clothing_item.map(function(item){
+    	return item.replace("*", "");
+    })
+    return clean_clothing_item;
 }
 
 
 /* PRINT RESULT */
 console.log("--> Finished running clean.js");
+console.log(clean(dirty_clothes));
